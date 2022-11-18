11e17c8864e628630c23261a4e8a51277e911a81
CNPCNPCNP <clayh_94@hotmail.com>
2022-11-18 21:23:03 +1000

    Go to BETR and open first race

--- scraper.py
+++ scraper.py
@@ -6,22 +6,36 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 #url for example Betfair race. 
-url = 'https://www.betfair.com.au/exchange/plus/en/football/fifa-world-cup/winner-2022-betting-1.145970106' 
+url = 'https://betr.com.au/racebook#/racing/home/upcoming'
+CLICK = "arguments[0].click();"
 
 #selenium web driver to test accessing BETR site
 class BrowserController():
+
     def __init__(self, path: str) -> None:
         self.path = path
-        self.wd = webdriver.Chrome('chromedriver')
+
+        self.wd = webdriver.Chrome(path)
         self.wd.maximize_window() # For maximizing window
         self.wd.implicitly_wait(20) # gives an implicit wait for 20 seconds
         self.wd.get(url)
+
+    def get_price(self) -> float:
+        #search = self.wd.find_element(By.CLASS_NAME, "bet-button-price")
+        price = self.wd.find_element(By.XPATH, "//*[@id="+'"'+'"bm-content"'+"]/div[2]/div/div[2]/div[2]/div[4]/button/div")
         
-# I can't seem to toggle with this command to return where the odds for the horse are :(, currently yields an erorr
-#price_of_horsey= wd.find_element(By.XPATH, "//*[@id="+'"'+'"bm-content"'+"]/div[2]/div/div[2]/div[2]/div[4]/button/div")
+        return price
+
+    def goto_next_race(self):
+        next_race = self.wd.find_element(By.XPATH, '//*[@id="bm-content"]/div[2]/div/div[2]/div[2]/div[1]')
+        self.wd.execute_script(CLICK, next_race)
+        print(self.wd.title)
 
 if __name__ == "__main__":
     load_dotenv()
     path = os.environ.get("PATH")
     browserController = BrowserController(path)
+    #price = browserController.get_price()
+    #print(price)
+    browserController.goto_next_race()
     time.sleep(10)
