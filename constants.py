0fd00e4d719140e5dca295adced70e4de938dd04
CNPCNPCNP <clay.hart@uqconnect.edu.au>
2023-01-09 16:41:09 +1000

    Today's results

--- constants.py
+++ constants.py
@@ -10,7 +10,7 @@ TROT_ICON = 'M14.9389'
 GREYHOUND_ICON = 'M2.73859'
 WIN_MARKET_REGEX = re.compile(r'^R\d+$')
 PRICE_PROJECTION = filters.price_projection(price_data = filters.price_data(ex_best_offers=True))
-RUN_TIME_MINUTES = 500
+RUN_TIME_MINUTES = 600
 
 #First value is BETR venue name, second value is betfair venue name
 VENUES = {# Greyhound races
