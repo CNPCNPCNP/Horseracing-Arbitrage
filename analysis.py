f860c3371ab538384f4661c335273db013b044c9
Omegago94 <benedict.gordon@uqconnect.edu.au>
2023-01-15 12:36:59 +1000

    updated historical analysis for US markets

--- analysis.py
+++ analysis.py
@@ -48,26 +48,32 @@ betfair_data.drop('PPTRADEDVOL', inplace=True, axis=1)
 betfair_data.drop('IPTRADEDVOL', inplace=True, axis=1)
 betfair_data.drop('SELECTION_ID', inplace=True, axis=1)
 betfair_data.drop('EVENT_NAME', inplace=True, axis=1)
-betfair_data.drop('MENU_HINT', inplace=True, axis=1)
 
 betfair_data.reset_index(inplace=True)
 betfair_data.drop('EVENT_ID', inplace=True, axis=1)
 betfair_data = betfair_data.replace(regex=[r'\d+\.\s'], value='')
 betfair_data['EVENT_DT'] = betfair_data['EVENT_DT'].apply(lambda x: x.split()[0])
+betfair_data['MENU_HINT'] = betfair_data['MENU_HINT'].apply(lambda x: " ".join(x.split()[2:-3]))
 
 all_bets.to_csv(f'analysis/results/all_bets.csv')
 betfair_data.to_csv(f'analysis/results/betfair_data.csv')
 
-all_bets = pd.merge(all_bets, betfair_data, left_on=['Horse', 'Datetime'], right_on=['SELECTION_NAME', 'EVENT_DT'])
+all_bets = pd.merge(all_bets, betfair_data, left_on=['Horse', 'Datetime', 'Venue'], right_on=['SELECTION_NAME', 'EVENT_DT', 'MENU_HINT'])
 all_bets["Turnover"] = (all_bets["Price"] / all_bets["BSP"] * 100)
+all_bets['Expected Value'] = ((all_bets["Price"] - 1) * (1 / all_bets['BSP']) + (-1 * (1 - 1 / all_bets['BSP'])))
+
 all_bets.drop('EVENT_DT', inplace=True, axis=1)
 all_bets.drop('SELECTION_NAME', inplace=True, axis=1)
+all_bets.drop('MENU_HINT', inplace=True, axis=1)
+all_bets.fillna(1, inplace=True)
+
+all_bets['Datetime'] = pd.to_datetime(all_bets['Datetime'], format='%d-%m-%Y')
 
 all_bets.to_csv(f'analysis/results/all_bets2.csv')
 
+recent = all_bets.loc[all_bets['Datetime'] >= pd.Timestamp(year=2023, month=1, day=8)]
+yesterday = all_bets.loc[all_bets['Datetime'] == pd.Timestamp('today').floor('D') - pd.Timedelta(1, unit='D')]
+
 print(all_bets['Turnover'].mean(), len(all_bets))
-
-recent = all_bets.loc[(all_bets['Datetime'] == '09-01-2023') | (all_bets['Datetime'] == '08-01-2023')]
 print(recent['Turnover'].mean(), len(recent))
-
-
+print(yesterday['Turnover'].mean(), len(yesterday))
